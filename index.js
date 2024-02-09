99248b200a8ed4d2a19a1e4e88ec5e710373d1a1
keithchasen <keithchasen89@gmail.com>
2024-02-09 14:02:01 +0100

    basic users route

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const { createServer } = require('node:http');
+const cors = require('cors');
+const userRoutes = require('./routes/user');
 
 const app = express();
 const server = createServer(app);
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
     res.send('Welcome to the socket API');
