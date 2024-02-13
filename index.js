0d974f82cad82eb3a40011bfb8999af94e17e3ca
keithchasen <keithchasen89@gmail.com>
2024-02-13 13:30:50 +0100

    basic socket set up

--- index.js
+++ index.js
@@ -4,7 +4,17 @@ const userRoutes = require('./routes/user');
 const chatRoutes = require('./routes/chat');
 const messagesRoutes = require('./routes/messages');
 
+const { Server } = require('socket.io');
+const { createServer } = require('node:http');
+
 const app = express();
+const server = createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: '*'
+  }
+});
 
 app.use(cors());
 app.use(express.json());
@@ -17,6 +27,10 @@ app.get('/', (req, res) => {
     res.send('Welcome to the socket API');
 });
 
-app.listen(1200, () => {
+io.on('connection', socket => {
+  console.log('socket connected', socket.id);
+});
+
+server.listen(1200, () => {
   console.log('server running at http://localhost:1200');
 });
